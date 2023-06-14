109d7fb03c36420e42b8fae1198245c1542cefa7
Caleb <cheruiyotdanielcaleb@gmail.com>
2023-06-14 12:51:06 +0300

    added job_link

--- scraper/models.py
+++ scraper/models.py
@@ -7,7 +7,9 @@ from django.db import models
 class JbDate(models.Model):
     job_title =models.CharField(max_length=1000)
     scraped_date = models.CharField(max_length=1000)
-    job_image = models.CharField(max_length=1000)
+    job_image = models.URLField(max_length=5000)
+    job_link = models.URLField(max_length=5000, default='')
+    job_function = models.CharField(max_length=1000, default='')
 
 
 def __str__(self):
--- scraper/views.py
+++ scraper/views.py
@@ -14,10 +14,17 @@ divs = soup.find_all('div',class_="mx-5 md:mx-0 flex flex-wrap col-span-1 mb-5 b
 for job in divs:
     save=JbDate()
     job_title = job.find('div',class_="flex items-center").find('p',class_='text-lg font-medium break-words text-link-500').get_text().strip()
+    job_link = job.find('div', class_="flex items-center").find('a', class_='relative mb-3 text-lg font-medium break-words focus:outline-none metrics-apply-now text-link-500 text-loading-animate')['href']
+    # job_function = job.find('div', class_="flex items-center").find('p', class_='text-sm text-gray-500 text-loading-animate inline-block').get_text().strip()
+    # job_image = job.find('div', class_="flex justify-center items-center p-1 w-[100px] h-[100px] align-middle bg-white rounded-md border-2 border-gray-300").find('img', class_='w-full h-full object-contain transition-opacity duration-200 ease-in rounded')['href']
     dates = job.find('div',class_="flex flex-row items-start items-center px-5 py-3 w-full border-t border-gray-300").find('p', class_='ml-auto text-sm font-normal text-gray-700 text-loading-animate').get_text().strip()
 
     if dates is not None:
         save.scraped_date=dates
+
+    # save.job_function=job_function
+    # save.job_image=job_image
+    save.job_link=job_link
     save.job_title=job_title
     save.save()
 class JobViewSet(viewsets.ModelViewSet):
